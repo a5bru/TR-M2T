467580a3babc1c8910c2295651d26a3c57edbf77
root <root@vmi2234232.contaboserver.net>
2025-01-03 10:48:20 +0000

    wip

--- connection_hub.py
+++ connection_hub.py
@@ -7,34 +7,68 @@ import selectors
 import time
 import threading
 import base64
-import queue
 import threading
 import string
 import os
+import queue
 import random
-import pickle
-
 import paho.mqtt.client as mqtt
 
 from urllib.parse import urlparse
 
+WORKERS = int(os.environ.get("MQTT_HUB_WORKERS", "2"))
+ZMQ_PULL_PORT = int(os.environ.get("ZMQ_PULL_PORT", "6969"))
+MAX_INACTIVE_COUNT = int(os.environ.get("HUB_MAX_INACTIVE_COUNT", "10"))
+
 context = zmq.Context()
 selector = selectors.DefaultSelector()
 connections = {}
-#data_queue = queue.Queue()
-
-WORKERS = int(os.environ.get("MQTT_HUB_WORKERS", "2"))
-
-ZMQ_PULL_PORT = int(os.environ.get("ZMQ_PULL_PORT", "6969"))
+inactive = {}
 
 
-def generate_random_string(length):
+class DataConnection:
+    def __init__(self, idx: int, url: str, socket: socket.socket, active: bool = True):
+        self.idx = idx
+        self.url = url
+        self.active = active
+        self.socket = socket
+        self.inactive_count = 0
+
+
+def update_mountpoint(id, name=None, connection_string=None, active=None):
+    conn = sqlite3.connect('mountpoints.db')
+    cursor = conn.cursor()
+    update_fields = []
+    params = []
+
+    if name is not None:
+        update_fields.append("name = ?")
+        params.append(name)
+    if connection_string is not None:
+        update_fields.append("connection_string = ?")
+        params.append(connection_string)
+    if active is not None:
+        update_fields.append("active = ?")
+        params.append(active)
+    
+    if update_fields:
+        params.append(id)
+        cursor.execute(f'''
+            UPDATE mountpoints 
+            SET {', '.join(update_fields)} 
+            WHERE id = ?
+        ''', params)
+        conn.commit()
+    conn.close()
+
+
+def generate_random_string(length: int):
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
 
 
-def create_tcp_client(url):
+def create_tcp_client(url: str):
 
     # Parse url info
     o = urlparse(url)
@@ -45,20 +79,12 @@ def create_tcp_client(url):
 
     # Create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (host, port)  # Replace with your server's IP and port
+    server_address = (host, port)
+    seconds = 1.0 
 
     try:
         client_socket.connect(server_address)
 
-    except BlockingIOError:
-        # This is expected for non-blocking sockets
-        pass
-
-    except Exception as e:
-        print(e)
-        return -1
-
-    try:
         request = f"GET {path} HTTP/1.0\r\n"
         request += "User-Agent: Ntrip N2Mqtt/v0.1\r\n"
         request += "Connection: close\r\n"
@@ -66,12 +92,11 @@ def create_tcp_client(url):
         request += f"Authorization: Basic {auth}\r\n"
         request += "\r\n"
         client_socket.sendall(request.encode()) 
-        seconds = 2.0 
         readable, _, _ = select.select([client_socket, ], [], [], seconds)
         if not readable:
-            assert False, f"E: {path}: No Response within {seconds} secs."
+            assert False, f"E: {path}: No Response within {seconds} sec(s)."
         data = client_socket.recv(1024)
-
+        
         # make sure request was not denied
         assert not data.startswith(b"HTTP"), f"E: {path}: Response Error {data[:20].decode()}.."
         # make sure mountpoint is alive
@@ -83,7 +108,10 @@ def create_tcp_client(url):
         print(e, file=sys.stderr)
         return None
 
-    client_socket.setblocking(0)  # Set socket to non-blocking
+    except Exception as e:
+        print(e)
+        return None
+
     return client_socket
 
 
@@ -96,50 +124,90 @@ def fetch_active_mountpoints():
     return rows
 
 
-def check_mountpoints():
+def creation_thread(id: int, connection_string: str):
+    o = urlparse(connection_string)
+    print(f"I: {o.path}: Opening connection")
+    conn = create_tcp_client(connection_string)
+    if conn is not None:
+        fd = conn.fileno()
+        selector.register(conn, selectors.EVENT_READ)
+        connections[fd] = DataConnection(idx=id, url=connection_string, socket=conn, active=True) 
+        if o.path in inactive_count:
+            inactive_count.pop(o.path)
+    else:
+        if o.path not in inactive_count:
+            inactive_count[o.path] = 0
+
+        inactive_count[o.path] += 1
+
+        if inactive_count[o.path] > MAX_INACTIVE_COUNT:
+            # TODO disable mountpoint
+            update_mountpoint(id, active=False)
+ 
+
+
+def check_mountpoints(sock):
+
     while True:
+
         print("Check for active mountpoints")
         active_mountpoints = fetch_active_mountpoints()
         active_ids = set(mp[0] for mp in active_mountpoints)
 
-        for conn in list(connections.keys()):
-            id = connections[conn]["id"]
-            if id not in active_ids:
-                o = urlparse(connections[conn]["url"])
+        # shut down active stream
+        for fd in list(connections.keys()):
+            if connections[fd].idx not in active_ids:
+                o = urlparse(connections[fd].url)
                 print(f"I: {o.path}: Closing connection")
-                selector.unregister(conn)
-                conn.close()
-                del connections[conn]
+                sock.sendall(f"{fd}:".encode())
+
+        # active stream
+        active_streams = []
+
+        for fd in list(connections.keys()):
+            active_streams.append(connections[fd].idx)
 
         for id, connection_string in active_mountpoints:
-            if id not in connections:
-                o = urlparse(connection_string)
-                print(f"I: {o.path}: Opening connection")
-                conn = create_tcp_client(connection_string)
-                if conn != -1:
-                    selector.register(conn, selectors.EVENT_READ)
-                    connections[conn] = {"id": id, "url": connection_string}
+            if id not in active_streams:
+                creation_thread(id, connection_string)
 
         time.sleep(10)
 
 
-def handle_events():
+def handle_events(sock):
 
     sender = context.socket(zmq.PUSH)
     sender.bind(f"tcp://*:{ZMQ_PULL_PORT}")
+    selector.register(sock, selectors.EVENT_READ)
 
     while True:
+
         events = selector.select(timeout=None)
+
         for key, mask in events:
             conn = key.fileobj
             data = conn.recv(1024)
-            if data:
-                sender.send_pyobj((connections[conn]["id"], data))
-                #data_queue.put((connections[conn]["id"], data))
+
+            if conn == sock:
+                # remove a connection
+                for fd_b in sock.recv(1024).split(b":"):
+                    fd = int(fd_b)
+                    if fd in connections:
+                        selector.unregister(connections[fd].socket)
+                        connections[fd].active = False
+                        connections[fd].socket.close()
+                        del connections[fd]
+
             else:
-                selector.unregister(conn)
-                conn.close()
-                del connections[conn]
+                fd = conn.fileno()
+                if data:
+                    sender.send_pyobj((fd, data))
+                else:
+                    selector.unregister(conn)
+                    conn.close()
+                    if fd in connections:
+                        connections[fd].active = False
+                        del connections[fd]
 
         time.sleep(0.0001)
 
@@ -166,29 +234,31 @@ def worker(w_id: int, url: str):
     print(f"Started MQTT client {mqtt_client_id}")
 
     while True:
-        #item = data_queue.get()
-        #if item is None:
-        #    break
-        #conn_sock, data = item
-        #data_queue.task_done()
-        conn_id, data = receiver.recv_pyobj()
 
-        for conn_sock in list(connections.keys()):
-            if conn_id == connections[conn_sock]["id"]:
-                o2 = urlparse(connections[conn_sock]["url"])
+        fd, data = receiver.recv_pyobj()
+
+        if fd in list(connections.keys()):
+            try:
+                url = connections[fd].url
+                o2 = urlparse(url)
                 p2 = o2.path[1:]
+
                 topic = f"s2d/osr/{p2}/rtcm"
-                #assert len(data) > 0, f"E: {topic}: Empty response"
                 if len(data) > 0:
                     # Publish received data to MQTT
                     mqtt_client.publish(topic, data)
-                break;
+
+            except Exception as e:
+                print(e)
+
         time.sleep(0.000000001)
 
     mqtt_client.loop_stop()  # Stop the MQTT client loop
 
 
 def main():
+
+    sock1, sock2 = socket.socketpair()
 
     # start worker threads
     MQTT_HOST = os.environ.get("MQTT_HOST", "localhost")
@@ -203,12 +273,12 @@ def main():
     time.sleep(3)
 
     # start checker thread
-    db_thread = threading.Thread(target=check_mountpoints)
+    db_thread = threading.Thread(target=check_mountpoints, args=(sock1,))
     db_thread.daemon = True
     db_thread.start()
 
     # start selector
-    handle_events()
+    handle_events(sock2)
 
 
 if __name__ == "__main__":
